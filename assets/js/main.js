a614bf2301de814ea12f8d91281c0404e85e12d9
DoKieu123 <kieu.dtt.2002@gmail.com>
2023-06-14 14:45:41 +0700

    commit.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,23 @@
+//nút hiện thị menu mobie 
 var btnMenu = document.querySelector('.menu_none')
+//menu moibe
 var listMenu = document.querySelector('.menu_list--mobie')
+//  nut x đóng menu mobie
 var btnClose = document.querySelector('.btl_close')
-console.log(btnMenu);
-console.log(listMenu)
+
+//
+var body = document.getElementById('app')
+var opacity = body.querySelectorAll(':not(.menu_list--mobie)')
+console.log(opacity);
+// console.log(btnMenu);
+// console.log(listMenu)
 
 btnMenu.addEventListener('click',function(){
     listMenu.style.display = "block";
+    body.style.opacity ="0.5"
 })
 
 btnClose.addEventListener('click',function(){
     listMenu.style.display = "none";
+    body.style.opacity ="1"
 })
